7dfdcf018801eecef57ae8ee77050290a9d87a86
Guy Waldman <guywald1@gmail.com>
2017-10-11 23:28:36 +0300

    Add tests

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -6,13 +6,28 @@
 import * as assert from 'assert'
 
 import decorate from '../utils/decorate'
+import { defaultConfig } from '../utils/config'
+
+const { dash } = defaultConfig
+
+const toConfig : (string) => string = (line : string) => line.replace(/(-)/g, () => dash)
 
 suite('Decoration Test', () => {
   // Defines a Mocha unit test
   test('should work with defaults', () => {
-    assert.equal(decorate('a', 1), 'A')
-    assert.equal(decorate('a', 6), '-- A --')
-    assert.equal(decorate('a', 5), '- A --')
-    assert.equal(decorate('aa', 5), '- AA -')
+    assert.equal(decorate('a', 3), toConfig(' A '))
+    assert.equal(decorate('a', 6), toConfig('-- A -'))
+    assert.equal(decorate('a', 7), toConfig('-- A --'))
+  })
+  test('should not uppercase text when option is disabled', () => {
+    assert.equal(decorate('a', 5), toConfig('- A -'))
+    assert.equal(decorate('a', 5, { shouldUppercase: true }), toConfig('- A -'))
+    assert.equal(decorate('a', 5, { shouldUppercase: false }), toConfig('- a -'))
+  })
+  test('should properly apply padding and dash', () => {
+    assert.equal(decorate('foo', 11), '--- FOO ---')
+    assert.equal(decorate('foo', 11, { padding: 2 }), '--  FOO  --')
+    assert.equal(decorate('foo', 11, { dash: '~', padding: 2 }), '~~  FOO  ~~')
+    assert.equal(decorate('Foo', 87, { shouldUppercase: false, padding: 2 }), toConfig(`${'-'.repeat(40)}  Foo  ${'-'.repeat(40)}`))
   })
 })
