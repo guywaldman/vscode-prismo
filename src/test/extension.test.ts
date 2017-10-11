15beb212db11e008f81b101e1cc4ea5548f63c9f
Guy Waldman <guywald1@gmail.com>
2017-10-11 19:27:45 +0300

    Add basic tests

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -3,20 +3,16 @@
 // Please refer to their documentation on https://mochajs.org/ for help.
 //
 
-// The module 'assert' provides assertion methods from node
-import * as assert from 'assert';
+import * as assert from 'assert'
 
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
-import * as vscode from 'vscode';
-import * as myExtension from '../extension';
+import decorate from '../utils/decorate'
 
-// Defines a Mocha test suite to group tests of similar kind together
-suite("Extension Tests", () => {
-
-    // Defines a Mocha unit test
-    test("Something 1", () => {
-        assert.equal(-1, [1, 2, 3].indexOf(5));
-        assert.equal(-1, [1, 2, 3].indexOf(0));
-    });
-});
+suite('Decoration Test', () => {
+  // Defines a Mocha unit test
+  test('should work with defaults', () => {
+    assert.equal(decorate('a', 1), 'A')
+    assert.equal(decorate('a', 6), '-- A --')
+    assert.equal(decorate('a', 5), '- A --')
+    assert.equal(decorate('aa', 5), '- AA -')
+  })
+})
