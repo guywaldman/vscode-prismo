a824cf00a62b4a8f68667270ec0635abf987eda3
Guy Waldman <guywald1@gmail.com>
2018-09-01 01:09:54 +0300

    Add user prompt for defining comment pattern

--- src/utils/comments/index.ts
+++ src/utils/comments/index.ts
@@ -1,4 +1,5 @@
-import patternFromPresets from './patternFromPresets';
+import { window } from "vscode";
+import patternFromPresets from "./patternFromPresets";
 
 /**
  * Resolves difference in length from a string pattern.
@@ -11,15 +12,29 @@ const lengthDiffFromCommentPattern: (string) => number = (
   commentPattern: string
 ) => commentPattern.length - 2;
 
-function commentPatternFromLanguage(languageId: string) : Promise<string> {
-  // TODO: handle non-existant
-  const tryPatternFromPresets = patternFromPresets(languageId);
-  if (tryPatternFromPresets) {
-    return Promise.resolve(tryPatternFromPresets);
+/**
+ * Resolves a comment pattern from the languageId in the active editor.
+ * In case the languageId is not inside the presets (defined by the extension),
+ * the user will input their own style of comment.
+ *
+ * > The setting will be saved in the configuration for future uses.
+ * @param languageId language id in the editor
+ * @return {Promise<string>} the comment pattern for the languageId
+ */
+async function commentPatternFromLanguage(languageId: string): Promise<string> {
+  // const tryPatternFromPresets = patternFromPresets(languageId);
+  // if (tryPatternFromPresets) {
+  //   return Promise.resolve(tryPatternFromPresets);
+  // }
+  let input = "";
+  while (!input.includes("%s")) {
+    input = await window.showInputBox({
+      prompt:
+        "This language is not recognized. Enter a comment pattern for this language in the style `// %s` where %s is the comment text",
+      value: "// %s"
+    });
   }
-  // TODO: handle user prompt to add comment pattern
-  return Promise.resolve("// %s");
+  return Promise.resolve(input);
 }
 
-
-export { lengthDiffFromCommentPattern, commentPatternFromLanguage  };
+export { lengthDiffFromCommentPattern, commentPatternFromLanguage };
